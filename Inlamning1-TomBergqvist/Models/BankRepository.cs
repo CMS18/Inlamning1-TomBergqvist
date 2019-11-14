36de3155e79967c50c13df1b435f9d9948d96c12
Tom Bergqvist <bergqvist.tom@gmail.com>
2019-11-14 22:38:10 +0100

    deposit and withdraw added

--- Inlamning1-TomBergqvist/Models/BankRepository.cs
+++ Inlamning1-TomBergqvist/Models/BankRepository.cs
@@ -9,5 +9,50 @@ namespace Inlamning1_TomBergqvist.Models
     {
         public List<Customer> Customers { get; set; } = new List<Customer>();
         public List<Account> Accounts { get; set; } = new List<Account>();
+
+        public string Withdraw(int id, decimal amount)
+        {
+            var account = Accounts.SingleOrDefault(a => a.Id == id);
+
+            if(account == null)
+            {
+                return $"The account {id} does not exist.";
+            }
+            else
+            {
+                if (amount <= 0)
+                {
+                    return "The amount has to be positive.";
+                }
+                if (account.Balance < amount)
+                {
+                    return $"The balance on account {id} is too low.";
+                }
+                else
+                {
+                    account.Balance -= amount;
+                    return $"The account {id} now has {account.Balance} after withdrawing {amount}.";
+                }
+            }
+        }
+        
+        public string Deposit(int id, decimal amount)
+        {
+            var account = Accounts.SingleOrDefault(a => a.Id == id);
+
+            if (account == null)
+            {
+                return $"The account {id} does not exist.";
+            }
+            else
+            {
+                if (amount <= 0)
+                {
+                    return "The amount has to be positive.";
+                }
+                account.Balance += amount;
+                return $"The account {id} now has {account.Balance} after depositing {amount}.";
+            }
+        }
     }
 }
