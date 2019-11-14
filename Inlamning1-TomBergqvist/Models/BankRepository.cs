a9866354f170cdf77a2f96cf25aa540437a08645
Tom Bergqvist <bergqvist.tom@gmail.com>
2019-11-14 16:21:44 +0100

    Added empty classes

---
+++ Inlamning1-TomBergqvist/Models/BankRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlamning1_TomBergqvist.Models
+{
+    public class BankRepository
+    {
+    }
+}
